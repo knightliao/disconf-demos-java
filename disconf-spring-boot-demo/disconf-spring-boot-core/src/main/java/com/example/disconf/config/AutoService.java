e44ebcbd5d7676d6b44c4840b1d0e4f135ff5a9d
knightliao <knightliao@gmail.com>
2015-11-13 22:48:50 +0800

    update to 4.1.7

---
+++ disconf-spring-boot-demo/disconf-spring-boot-core/src/main/java/com/example/disconf/config/AutoService.java
@@ -0,0 +1,17 @@
+package com.example.disconf.config;
+
+/**
+ * Created by knightliao on 15/3/19.
+ */
+public class AutoService {
+
+    private String auto;
+
+    public String getAuto() {
+        return auto;
+    }
+
+    public void setAuto(String auto) {
+        this.auto = auto;
+    }
+}
--- disconf-spring-boot-demo/disconf-spring-boot-core/src/main/java/com/example/disconf/task/DisconfDemoTask.java
+++ disconf-spring-boot-demo/disconf-spring-boot-core/src/main/java/com/example/disconf/task/DisconfDemoTask.java
@@ -1,21 +1,21 @@
 package com.example.disconf.task;
 
-import com.example.disconf.config.SimpleConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.disconf.config.SimpleConfig;
 
 /**
  * 定时输出配置修改
  */
-public class DisconfDemoTask implements Runnable{
+public class DisconfDemoTask implements Runnable {
 
     protected static final Logger LOGGER = LoggerFactory
             .getLogger(DisconfDemoTask.class);
 
     private SimpleConfig simpleConfig;
 
-    public DisconfDemoTask(SimpleConfig simpleConfig){
+    public DisconfDemoTask(SimpleConfig simpleConfig) {
         this.simpleConfig = simpleConfig;
     }
 
@@ -24,7 +24,7 @@ public class DisconfDemoTask implements Runnable{
 
             while (true) {
                 Thread.sleep(5000);
-                LOGGER.info("simple config ==> host:{}, port:{} ",simpleConfig.getHost(),simpleConfig.getPort());
+                LOGGER.info("simple config ==> host:{}, port:{} ", simpleConfig.getHost(), simpleConfig.getPort());
             }
 
         } catch (Exception e) {
