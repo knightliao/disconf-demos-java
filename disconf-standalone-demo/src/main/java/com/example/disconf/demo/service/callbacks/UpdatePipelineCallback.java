e52099f6f679576d1a83998f8bf3a1afab7db0da
knightliao <knightliao@gmail.com>
2016-03-27 11:46:25 +0800

    read file when content update

--- disconf-standalone-demo/src/main/java/com/example/disconf/demo/service/callbacks/UpdatePipelineCallback.java
+++ disconf-standalone-demo/src/main/java/com/example/disconf/demo/service/callbacks/UpdatePipelineCallback.java
@@ -1,5 +1,10 @@
 package com.example.disconf.demo.service.callbacks;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.stereotype.Service;
 
 import com.baidu.disconf.client.common.update.IDisconfUpdatePipeline;
@@ -10,8 +15,17 @@ import com.baidu.disconf.client.common.update.IDisconfUpdatePipeline;
 @Service
 public class UpdatePipelineCallback implements IDisconfUpdatePipeline {
 
+    static String readFile(String path, Charset encoding)
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
     public void reloadDisconfFile(String key, String filePath) throws Exception {
+
         System.out.println(key + " : " + filePath);
+
+        System.out.println(readFile(filePath, Charset.defaultCharset()));
     }
 
     public void reloadDisconfItem(String key, Object content) throws Exception {
