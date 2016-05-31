04db21b276dbf4c78113f5b4b0481c7c67f9ddbb
knightliao <knightliao@gmail.com>
2016-05-31 14:39:54 +0800

    更改目录 结构

--- disconf-standalone-demo/src/main/java/com/example/disconf/demo/config/CodeConfig.java
+++ disconf-standalone-demo/src/main/java/com/example/disconf/demo/config/CodeConfig.java
@@ -14,9 +14,20 @@ public class CodeConfig {
 
     private String codeError = "";
 
+    private boolean routerRateLimitEnable;
+
     @DisconfFileItem(name = "syserror.paramtype", associateField = "codeError")
     public String getCodeError() {
         return codeError;
+    }
+
+    @DisconfFileItem(name = "proxy.routerRateEnable", associateField = "routerRateLimitEnable")
+    public boolean isRouterRateLimitEnable() {
+        return routerRateLimitEnable;
+    }
+
+    public void setRouterRateLimitEnable(boolean routerRateLimitEnable) {
+        this.routerRateLimitEnable = routerRateLimitEnable;
     }
 
     public void setCodeError(String codeError) {
