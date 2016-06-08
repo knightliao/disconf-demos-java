eb45b98a77e90c6e5d8f5912576b29e042031c6e
knightliao <knightliao@gmail.com>
2016-06-08 14:26:56 +0800

    use .

--- disconf-standalone-demo/deploy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-java -Dlogback.configurationFile=logback.xml \
-    -Djava.ext.dirs=lib \
-    -Xms128M -Xmx256M -cp .:disconf-standalone-demo.jar \
-    com.example.disconf.demo.DisconfDemoMain
